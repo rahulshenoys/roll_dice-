44a35759a1900dd71578aacda8b23b4c42a38cfb
rahulshenoy1 <rahul.shenoy@exathought.com>
2024-05-08 13:48:59 +0530

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:roll_dice/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 29, 4, 71),
+          Color.fromARGB(255, 43, 7, 105),
+        ),
+      ),
+    ),
+  );
+}
